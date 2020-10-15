c11382a2b2cb1324a28ca9633fff2a7b50e162c2
prvsct <psroveri@gmail.com>
2020-10-15 17:10:48 -0300

    Corrects ifs_data.R as to properly export the function.

--- R/ifs_data.R
+++ R/ifs_data.R
@@ -3,8 +3,8 @@
 #' \code{ifs_data} downloads data from IFS.
 #' A simplification of \code{\link[imfr]{imf_data}}, which sets \code{database_id} to \code{"IFS"}, \code{times} to \code{3} and both \code{return_raw} and \code{print_url} to \code{FALSE}.
 #'
-#' @param indicator A character, the IFS database indicator as in \code{\link{ifs_indicators}}.
-#' @param country A character, the ISO code of the country as in \code{\link{ifs_countries}}.
+#' @param indicator A character, the IFS database indicator as in \code{ifs_indicators} dataframe.
+#' @param country A character, the ISO code of the country as in \code{ifs_countries} dataframe.
 #' @param start A number, the year for which you would like to start gathering the data.
 #' @param end A number, the year for which you would like to start gathering the data.
 #' @param freq A character, indicating the series frequency. With 'A' for annual, 'Q' for quarterly, and 'M' for monthly.
@@ -12,6 +12,7 @@
 #' Please see also \code{\link[imfr]{imf_data}} for a more throughout explanation.
 #' @examples
 #' ifs_data(indicator = "ENDE_XDC_USD_RATE", country = c("UK","BR"), start = 1999, end = 2020, freq = "M")
+#' @export
 
 
 ifs_data <- function(indicator,
