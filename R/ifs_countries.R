cc6234fefba577c37958e56bc8b7551f6be50d4d
prvsct <psroveri@gmail.com>
2020-10-15 17:27:14 -0300

    Creates documentation for ifs_countries and ifs_indicators.

---
+++ R/ifs_countries.R
@@ -0,0 +1,9 @@
+#' ifs_countries
+#' A data frame containing the name of 248 countries and its ISO 3166-1 alpha-2 codes
+#' One can use this data frame to search for the correct ISO code of the country he wants do download data using \code{\link{ifs_data}}.
+#'
+#' @format A data frame with 248 rows and 2 columns.
+#'
+#' @source \url{https://datahub.io/core/country-list}
+
+"ifs_countries"
---
+++ R/ifs_indicators.R
@@ -0,0 +1,8 @@
+#' ifs_indicatora
+#' A data frame containing the name and description of all indicators avaible for download at the IFS.
+#' One can use this data frame to search for the correct code of the indicator he wants do download data using \code{\link{ifs_data}}.
+#'
+#' @format A data frame with 1668 rows and 2 columns.
+#'
+
+"ifs_indicators"
