398e2a4cc93dddefb12efa9c68dcb128d6a9dbb3
prvsct <psroveri@gmail.com>
2021-02-19 13:23:12 -0300

    COrrectly orders names from iso_converter.

--- R/iso_converter.R
+++ R/iso_converter.R
@@ -13,7 +13,7 @@ iso_converter <- function(names){
   if( is.vector(names, mode ="character") ){
     if( all(names %in% surexr::ifs_countries$Name) ){
       names <- surexr::ifs_countries$Code[surexr::ifs_countries$Name %in% names]
-      return(names)
+      return(names[order(names)])
     } else{
       wrong_name <- names[which(!names%in% surexr::ifs_countries$Name)]
       print(paste0("Error: ",wrong_name," is not a valid name. Have you checked ifs_countries?"))
